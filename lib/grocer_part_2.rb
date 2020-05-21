63908b11ded8fd28277f7b1727a0d1ddabe52749
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-21 21:46:05 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -6,14 +6,18 @@ def apply_coupons(cart, coupons)
     index_of_item_in_cart = cart.index(find_item_by_name_in_collection(coupon_item, cart))
     num_in_coupon = coupon[:num]
     num_in_cart = cart[index_of_item_in_cart][:count] 
-    num_applicable = (num_in_cart / num_in_coupon)*num_in_coupon 
+    num_applicable = (num_in_cart / num_in_coupon) * num_in_coupon 
     num_left = num_in_cart % num_in_coupon
     cart[index_of_item_in_cart][:count] = num_left 
     cart.push(cart[index_of_item_in_cart]) 
     cart[-1][:count] = num_applicable 
     cart[-1][:price] = coupon[:cost].to_f / num_in_coupon.to_f 
-    
+  end 
+  puts cart 
+  return cart 
 end
+
+
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
