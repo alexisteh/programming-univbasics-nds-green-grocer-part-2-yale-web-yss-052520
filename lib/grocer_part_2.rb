98bbe6bfb9f553ae385a96b8a07c57e7a230c646
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-21 22:01:22 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -31,7 +31,7 @@ coupons= [
 
 def apply_clearance(cart)
   cart.each do |itemhash|  
-    if itemhash[:clearance] = true 
+    if itemhash[:clearance] == true 
       itemhash[:price] = 0.8 * itemhash[:price]
     end 
   end 
