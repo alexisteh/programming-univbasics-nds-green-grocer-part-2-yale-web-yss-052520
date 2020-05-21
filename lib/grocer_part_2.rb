f8475642783796a084973f0ebc587339a4cabb39
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-21 21:50:39 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -9,7 +9,9 @@ def apply_coupons(cart, coupons)
     num_applicable = (num_in_cart / num_in_coupon) * num_in_coupon 
     num_left = num_in_cart % num_in_coupon
     cart[index_of_item_in_cart][:count] = num_left 
-    cart.push(cart[index_of_item_in_cart]) 
+    cart << cart[index_of_item_in_cart]
+    cart[-1][:item] += " W/COUPON"  
+    cart[-1][:price] = coupon[:cost].to_f / num_in_coupon.to_f 
   end 
   puts cart 
   return cart 
